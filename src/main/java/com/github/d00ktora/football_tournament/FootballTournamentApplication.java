c7c90512603e662a4369ded74e2861f0fadbbb5e
Stilyan Petrov <f.and.g.2019@gmail.com>
2026-03-01 10:25:16 +0200

    Upload Project

---
+++ src/main/java/com/github/d00ktora/football_tournament/FootballTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.github.d00ktora.football_tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTournamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/d00ktora/football_tournament/FootballTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.d00ktora.football_tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
