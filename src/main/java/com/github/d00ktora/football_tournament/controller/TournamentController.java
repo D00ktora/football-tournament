056717fc9e0a60a3e22d91c6732ae8c5ed414b29
Stilyan Petrov <f.and.g.2019@gmail.com>
2026-03-04 06:24:43 +0200

    Create Tournament Controller

---
+++ src/main/java/com/github/d00ktora/football_tournament/controller/TournamentController.java
@@ -0,0 +1,28 @@
+package com.github.d00ktora.football_tournament.controller;
+
+import com.github.d00ktora.football_tournament.entities.dto.TournamentResultDTO;
+import com.github.d00ktora.football_tournament.service.TournamentService;
+import com.github.d00ktora.football_tournament.service.sweep.enums.Mode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/")
+public class TournamentController {
+
+	private final TournamentService tournamentService;
+
+	@GetMapping("results")
+	public ResponseEntity<List<TournamentResultDTO>> results (@RequestParam Mode mode) {
+		List<TournamentResultDTO> resultDTOS = tournamentService.processTournament(mode);
+		return ResponseEntity.ok(resultDTOS);
+	}
+
+}
